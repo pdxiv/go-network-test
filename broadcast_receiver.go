95f750577acf771d4017d2349568916e3e8734b6
pdxiv <frank.stengard@gmail.com>
2018-07-09 17:12:25 +0200

    added config file support

--- broadcast_receiver.go
+++ broadcast_receiver.go
@@ -1,23 +1,42 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	reuse "github.com/libp2p/go-reuseport"
 	"log"
-	// "net"
+	"os"
 )
+
+const ConfigFile = "conf.json"
+
+type Configuration struct {
+	BroadcastAddress string
+}
 
 func main() {
 	_ = startSession()
 }
 
 func startSession() error {
+	// Load configuration from file
+	configuration := getConfiguration(ConfigFile)
+
+	fmt.Printf("'%s'\n", configuration.BroadcastAddress)
+
+	// If configuration undefined, set default value
+	if len(configuration.BroadcastAddress) == 0 {
+		configuration.BroadcastAddress = "0.0.0.0:9999"
+	}
+
 	// Listen to incoming UDP datagrams
-	pc, err := reuse.ListenPacket("udp", "192.168.0.18:9999")
+	pc, err := reuse.ListenPacket("udp", configuration.BroadcastAddress)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer pc.Close()
+
+	fmt.Println("Listening on", configuration.BroadcastAddress)
 
 	for {
 		//simple read
@@ -28,4 +47,16 @@ func startSession() error {
 		//simple write
 		// pc.WriteTo([]byte("Hello from client"), addr)
 	}
+}
+
+func getConfiguration(filename string) Configuration {
+	file, _ := os.Open(filename)
+	defer file.Close()
+	decoder := json.NewDecoder(file)
+	configuration := Configuration{}
+	err := decoder.Decode(&configuration)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+	return configuration
 }
