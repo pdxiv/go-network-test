f4cf189b850526c0d01a94ec73b424a247167ff1
pdxiv <frank.stengard@gmail.com>
2018-07-10 20:30:51 +0200

    some minor cosmetic changes

--- broadcast_receiver.go
+++ broadcast_receiver.go
@@ -11,7 +11,10 @@ import (
 const ConfigFile = "conf.json"
 
 type Configuration struct {
-	BroadcastAddress string
+	SequencerSinkAddress string
+	SequencerRiseAddress string
+	AppSinkAddress       string
+	AppRiseAddress       string
 }
 
 func main() {
@@ -22,30 +25,26 @@ func startSession() error {
 	// Load configuration from file
 	configuration := getConfiguration(ConfigFile)
 
-	fmt.Printf("'%s'\n", configuration.BroadcastAddress)
-
 	// If configuration undefined, set default value
-	if len(configuration.BroadcastAddress) == 0 {
-		configuration.BroadcastAddress = "0.0.0.0:9999"
+	if len(configuration.SequencerSinkAddress) == 0 {
+		configuration.SequencerSinkAddress = "0.0.0.0:9999"
 	}
+	fmt.Printf("'%s'\n", configuration.SequencerSinkAddress)
 
 	// Listen to incoming UDP datagrams
-	pc, err := reuse.ListenPacket("udp", configuration.BroadcastAddress)
+	pc, err := reuse.ListenPacket("udp", configuration.SequencerSinkAddress)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer pc.Close()
 
-	fmt.Println("Listening on", configuration.BroadcastAddress)
+	fmt.Println("Listening on", configuration.SequencerSinkAddress)
 
 	for {
-		//simple read
+		// Simple read
 		buffer := make([]byte, 512)
 		pc.ReadFrom(buffer)
 		fmt.Println("yeyyy, incoming udp datagram!", buffer)
-
-		//simple write
-		// pc.WriteTo([]byte("Hello from client"), addr)
 	}
 }
 
--- broadcast_sender.go
+++ broadcast_sender.go
@@ -1,16 +1,50 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"net"
+	"os"
 	"time"
 )
 
+const ConfigFile = "conf.json"
+
+type Configuration struct {
+	SequencerSinkAddress string
+	SequencerRiseAddress string
+	AppSinkAddress       string
+	AppRiseAddress       string
+}
+
 func main() {
-	destinationAddress, _ := net.ResolveUDPAddr("udp", "192.168.135.255:9999")
+	// Load configuration from file
+	configuration := getConfiguration(ConfigFile)
+
+	// If configuration undefined, set default value
+	if len(configuration.AppRiseAddress) == 0 {
+		configuration.AppRiseAddress = "192.168.0.255:9999"
+	}
+	fmt.Printf("'%s'\n", configuration.AppRiseAddress)
+
+	destinationAddress, _ := net.ResolveUDPAddr("udp", configuration.AppRiseAddress)
 	connection, _ := net.DialUDP("udp", nil, destinationAddress)
 	defer connection.Close()
+
 	for {
 		connection.Write([]byte("Hello"))
 		time.Sleep(1 * time.Second)
 	}
+}
+
+func getConfiguration(filename string) Configuration {
+	file, _ := os.Open(filename)
+	defer file.Close()
+	decoder := json.NewDecoder(file)
+	configuration := Configuration{}
+	err := decoder.Decode(&configuration)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+	return configuration
 }
