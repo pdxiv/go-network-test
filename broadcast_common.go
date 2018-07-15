13562bae2a7040d4581f5d26c31f9d8baec9d0b3
pdxiv <frank.stengard@gmail.com>
2018-07-15 19:09:50 +0200

    broke out common, shared functionality into broadcast_common.go

---
+++ broadcast_common.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"encoding/binary"
+	"encoding/json"
+	"fmt"
+	"net"
+	"os"
+)
+
+const PacketLimit = 10000 // If we're afraid of killing our network with the amount of load
+const ConfigFile = "conf.json"
+const BufferAllocationSize = 65507
+
+type Configuration struct {
+	SequencerSinkAddress string
+	SequencerRiseAddress string
+	AppSinkAddress       string
+	AppRiseAddress       string
+}
+
+type AppCommData struct {
+	// Actual data as native data types
+	Type                      uint16
+	PayloadSize               uint16
+	Id                        uint64
+	AppSequenceNumber         uint64
+	PreviousAppSequenceNumber uint64 // Used to keep track of sequence number gaps
+	// Temporary buffer storage for data
+	TypeBuffer              []byte
+	SizeBuffer              []byte
+	IdBuffer                []byte
+	AppSequenceNumberBuffer []byte
+	Payload                 []byte
+	// The actual data as bytes that will be sent over UDP
+	MasterBuffer []byte
+}
+
+// Initialize all the message parameters
+func initAppMessage(data *AppCommData) {
+	data.Type = 0
+	data.PayloadSize = 0
+	data.Id = 0
+	data.AppSequenceNumber = 0
+	data.PreviousAppSequenceNumber = 0
+	data.TypeBuffer = make([]byte, 2)
+	data.SizeBuffer = make([]byte, 2)
+	data.IdBuffer = make([]byte, 8)
+	data.AppSequenceNumberBuffer = make([]byte, 8)
+	data.Payload = make([]byte, 0, BufferAllocationSize)
+	data.MasterBuffer = make([]byte, 0, BufferAllocationSize)
+}
+
+func decodeAppMessage(data *AppCommData) {
+	data.PreviousAppSequenceNumber = data.AppSequenceNumber
+	data.Type = binary.BigEndian.Uint16(data.MasterBuffer[0:2])
+	data.PayloadSize = binary.BigEndian.Uint16(data.MasterBuffer[2:4])
+	data.Id = binary.BigEndian.Uint64(data.MasterBuffer[4:12])
+	data.AppSequenceNumber = binary.BigEndian.Uint64(data.MasterBuffer[12:20])
+	data.Payload = data.MasterBuffer[20 : 20+data.PayloadSize]
+
+	if data.PreviousAppSequenceNumber+1 != data.AppSequenceNumber {
+		fmt.Println("**************** Gap: ", data.PreviousAppSequenceNumber+1, "to", data.AppSequenceNumber-1)
+	}
+	fmt.Println("Datagram type:", data.Type)
+	fmt.Println("Datagram size:", data.PayloadSize)
+	fmt.Println("Datagram id:", data.Id)
+	fmt.Println("Datagram sequence number:", data.AppSequenceNumber)
+	fmt.Printf("Datagram payload: \"%s\"\n", string(data.Payload))
+}
+
+func sendAppMessage(data *AppCommData, connection *net.UDPConn) {
+	// Clear data buffers
+	data.MasterBuffer = data.MasterBuffer[:0] // Clear the byte slice send buffer
+
+	data.PayloadSize = uint16(len(data.Payload))
+
+	// Convert fields into byte arrays
+	binary.BigEndian.PutUint16(data.TypeBuffer, data.Type)
+	binary.BigEndian.PutUint16(data.SizeBuffer, data.PayloadSize)
+	binary.BigEndian.PutUint64(data.IdBuffer, data.Id)
+	binary.BigEndian.PutUint64(data.AppSequenceNumberBuffer, data.AppSequenceNumber)
+
+	// Add byte arrays to master output buffer
+	data.MasterBuffer = append(data.MasterBuffer, data.TypeBuffer...)
+	data.MasterBuffer = append(data.MasterBuffer, data.SizeBuffer...)
+	data.MasterBuffer = append(data.MasterBuffer, data.IdBuffer...)
+	data.MasterBuffer = append(data.MasterBuffer, data.AppSequenceNumberBuffer...)
+
+	// Add payload to master output buffer
+	data.MasterBuffer = append(data.MasterBuffer, data.Payload...)
+
+	connection.Write(data.MasterBuffer)
+	data.AppSequenceNumber++ // Increment App sequence number every time we've sent a datagram
+}
+
+func getConfiguration(filename string) Configuration {
+	file, _ := os.Open(filename)
+	defer file.Close()
+	decoder := json.NewDecoder(file)
+	configuration := Configuration{}
+	err := decoder.Decode(&configuration)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+	return configuration
+}
--- broadcast_receiver.go
+++ broadcast_receiver.go
@@ -1,40 +1,10 @@
 package main
 
 import (
-	"encoding/binary"
-	"encoding/json"
 	"fmt"
 	reuse "github.com/libp2p/go-reuseport"
 	"log"
-	"os"
 )
-
-const ConfigFile = "conf.json"
-const BufferAllocationSize = 65507
-
-type Configuration struct {
-	SequencerSinkAddress string
-	SequencerRiseAddress string
-	AppSinkAddress       string
-	AppRiseAddress       string
-}
-
-type AppCommData struct {
-	// Actual data as native data types
-	Type                      uint16
-	PayloadSize               uint16
-	Id                        uint64
-	AppSequenceNumber         uint64
-	PreviousAppSequenceNumber uint64 // Used to keep track of sequence number gaps
-	// Temporary buffer storage for data
-	TypeBuffer              []byte
-	SizeBuffer              []byte
-	IdBuffer                []byte
-	AppSequenceNumberBuffer []byte
-	Payload                 []byte
-	// The actual data as bytes that will be sent over UDP
-	MasterBuffer []byte
-}
 
 func main() {
 	_ = startSession()
@@ -67,49 +37,4 @@ func startSession() error {
 		pc.ReadFrom(data.MasterBuffer)
 		decodeAppMessage(&data)
 	}
-}
-
-func decodeAppMessage(data *AppCommData) {
-	data.PreviousAppSequenceNumber = data.AppSequenceNumber
-	data.Type = binary.BigEndian.Uint16(data.MasterBuffer[0:2])
-	data.PayloadSize = binary.BigEndian.Uint16(data.MasterBuffer[2:4])
-	data.Id = binary.BigEndian.Uint64(data.MasterBuffer[4:12])
-	data.AppSequenceNumber = binary.BigEndian.Uint64(data.MasterBuffer[12:20])
-	data.Payload = data.MasterBuffer[20 : 20+data.PayloadSize]
-
-	if data.PreviousAppSequenceNumber+1 != data.AppSequenceNumber {
-		fmt.Println("**************** Gap: ", data.PreviousAppSequenceNumber+1, "to", data.AppSequenceNumber-1)
-	}
-	fmt.Println("Datagram type:", data.Type)
-	fmt.Println("Datagram size:", data.PayloadSize)
-	fmt.Println("Datagram id:", data.Id)
-	fmt.Println("Datagram sequence number:", data.AppSequenceNumber)
-	fmt.Printf("Datagram payload: \"%s\"\n", string(data.Payload))
-}
-
-// Initialize all the message parameters
-func initAppMessage(data *AppCommData) {
-	data.Type = 0
-	data.PayloadSize = 0
-	data.Id = 0
-	data.AppSequenceNumber = 0
-	data.PreviousAppSequenceNumber = 0
-	data.TypeBuffer = make([]byte, 2)
-	data.SizeBuffer = make([]byte, 2)
-	data.IdBuffer = make([]byte, 8)
-	data.AppSequenceNumberBuffer = make([]byte, 8)
-	data.Payload = make([]byte, 0, BufferAllocationSize)
-	data.MasterBuffer = make([]byte, 0, BufferAllocationSize)
-}
-
-func getConfiguration(filename string) Configuration {
-	file, _ := os.Open(filename)
-	defer file.Close()
-	decoder := json.NewDecoder(file)
-	configuration := Configuration{}
-	err := decoder.Decode(&configuration)
-	if err != nil {
-		fmt.Println("error:", err)
-	}
-	return configuration
 }
