e5a67581803cd608fb4e89844d1e1b5b02df4715
pdxiv <frank.stengard@gmail.com>
2018-07-18 07:59:58 +0200

    made autoconfig.pl work on ubuntu 18.04 LTS too

--- autoconfig.pl
+++ autoconfig.pl
@@ -6,7 +6,7 @@ our $VERSION = '0.1.0';
 use utf8;
 use JSON;
 
-my $broadcast_address =  broadcast_for_interface( default_route_interface() );
+my $broadcast_address = broadcast_for_interface( default_route_interface() );
 
 my $json_data = {
     'AppRiseAddress'       => "$broadcast_address:9998",
@@ -36,8 +36,12 @@ sub broadcast_for_interface {
     my $current_interface = q{};
     my %interface_broadcast;
     foreach my $interface_line (@interface_info) {
-        if ($interface_line  =~ /^(\S+)/) { $current_interface = $1; }
-        if ($interface_line  =~ /Bcast:\s*(\S+)/) { $interface_broadcast{$current_interface} = $1; }
+        if ( $interface_line =~ /^([^: \n]+)/ ) {
+            $current_interface = $1;
+        }
+        if ( $interface_line =~ /(?: Bcast:| broadcast )\s*(\S+)/ ) {
+            $interface_broadcast{$current_interface} = $1;
+        }
     }
     return $interface_broadcast{$interface};
 }
