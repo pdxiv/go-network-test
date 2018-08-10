018bb32d77e2769318e4fa67ba070fcbf8a8f715
pdxiv <frank.stengard@gmail.com>
2018-08-10 07:56:50 +0200

    adding timestamping

--- stompy.go
+++ stompy.go
@@ -3,7 +3,10 @@ package main
 // stompy.go - skeleton of a test program that just sends timestamps, to
 // demonstrate what a "proper" rewheelify application should look like.
 
-import ()
+import (
+	"fmt"
+	"time"
+)
 
 const AppId = "STOMPY" // Wheel ID of the current application
 
@@ -13,10 +16,20 @@ func main() {
 	subscribedApp := []string{"AA", "AB", "AC"}
 	sender, receiver, _ := startSession(AppId, subscribedApp, configuration)
 
+	// Initialize time ticker for keeping track of when events happen
+	ticker := time.NewTicker(time.Second)
+	defer ticker.Stop()
+	latestTime := time.Now().UnixNano() // Initialize timestamp
+
 	// Select operation to wait for incoming messages
-	select {
-	case messageReceived := <-receiver:
-		sender <- messageReceived
+	for {
+		select {
+		case messageReceived := <-receiver:
+			sender <- messageReceived
+		case t := <-ticker.C:
+			latestTime = t.UnixNano()
+			fmt.Println("Current time:", latestTime)
+		}
 	}
 }
 
